8133ab94bd6eadb70bdecc524b746c6a34a182c8
George <eakarpov@yandex.ru>
2017-10-22 01:02:18 +0300

    Add notifier reducer to the store

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -3,16 +3,19 @@ import {counterReducer} from "./counter/index";
 import graphReducer from "./graph/reducers";
 import { State as CounterState } from "./counter/index";
 import {IGraphView} from '../models/graph';
+import {reducer as notifierReducer, INotifierStore} from "graphlabs.core.notifier";
 
 export interface RootState {
-  readonly graph: IGraphView,
-  counterState: CounterState
+  readonly graph: IGraphView;
+  counterState: CounterState;
+  notifier: INotifierStore;
 }
 
 
 const rootReducer: Reducer<RootState> = combineReducers<RootState>({
   counter: counterReducer,
-  graph: graphReducer
+  graph: graphReducer,
+  notifier: notifierReducer
 });
 
 export default rootReducer;

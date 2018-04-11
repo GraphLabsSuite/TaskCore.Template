04ee496dc83930d439a1dcc14cda90d40603acdb
George <eakarpov@yandex.ru>
2018-04-12 00:05:39 +0300

    all components export

--- src/export.ts
+++ src/export.ts
@@ -1,1 +1,17 @@
-export * from './components/TaskTemplate/TaskTemplate';
+import * as TaskTemplate from "./components/TaskTemplate/TaskTemplate";
+import * as CommonGraphAdapter from "./adapters/CommonGraphAdapter";
+import * as GraphVisualizer from "./components/GraphVisualizer/GraphVisualizer";
+import * as StudentMark from "./components/StudentMark/StudentMark";
+import * as TaskConsole from "./components/TaskConsole/TaskConsole";
+import * as TaskToolbar from "./components/TaskToolbar/TaskToolbar";
+import * as ToolButton from "./components/ToolButton/ToolButton";
+import * as ToolButtonList from "./components/ToolButtonList/ToolButtonList";
+
+module.exports.CommonGraphAdapter = CommonGraphAdapter;
+module.exports.TaskTemplate = TaskTemplate;
+module.exports.GraphVisualizer = GraphVisualizer;
+module.exports.StudentMark = StudentMark;
+module.exports.TaskConsole = TaskConsole;
+module.exports.TaskToolbar = TaskToolbar;
+module.exports.ToolButton = ToolButton;
+module.exports.ToolButtonList = ToolButtonList;

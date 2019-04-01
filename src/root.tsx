15d2a68c6797f0476856c8486b5f4b2161f9682f
eakarpov <eakarpov@yandex.ru>
2019-04-02 01:31:41 +0300

    1.0.12

--- src/root.tsx
+++ src/root.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import {Template} from './components/Template';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Template />, document.getElementById('root'));
 

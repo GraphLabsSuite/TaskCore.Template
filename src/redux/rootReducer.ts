cd471b74c787a6ff78949136cdb322ab8f46c782
eakarpov <eakarpov@yandex.ru>
2019-05-23 09:45:23 +0300

    matrix view in root state

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,12 +2,13 @@ import { combineReducers, Reducer } from 'redux';
 import graphReducer from './graph/reducers';
 import appReducer from './app/reducers';
 import matrixReducer from './matrix/reducers';
-import { IGraphView } from '..';
+import { IGraphView, IMatrixView } from '..';
 import {reducer as notifierReducer, INotifierStore } from 'graphlabs.core.notifier';
 import { App } from './app';
 
 export interface RootState {
   readonly graph: IGraphView;
+  readonly matrix: IMatrixView;
   readonly notifier: INotifierStore;
   readonly app: App;
 }
